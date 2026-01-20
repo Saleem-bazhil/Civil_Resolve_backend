977417a97f6c1696e5c6d683bf4982ba79f9e9e3
saleem <ab588538@gmail.com>
2026-01-20 15:19:12 +0530

    add the cors in main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin:"*",
+    methods:'GET,POST,PUT,PATCH,DELETE,OPTIONS',
+    allowedHeaders:"Content-Type,Authorization",
+  })
   const config = new DocumentBuilder()
     .setTitle('Civil Resolve')
     .setDescription('The Civil Resolve API description')
@@ -22,6 +27,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000,'0.0.0.0');
 }
 bootstrap();
