bfab4cfb5a3c2d9c57aedcaff408888b6da14acc
saleem <ab588538@gmail.com>
2026-02-03 22:49:08 +0530

    fix the image add to server

--- src/common/multer.config.ts
+++ src/common/multer.config.ts
@@ -4,13 +4,19 @@ import { extname } from 'path';
 export const multerConfig = {
   storage: diskStorage({
     destination: '/www/wwwroot/Civil_Resolve_backend/uploads',
-    filename: (_, file, cb) => {
-      const unique =
+    filename: (req, file, cb) => {
+      const uniqueName =
         Date.now() + '-' + Math.round(Math.random() * 1e9);
-      cb(null, unique + extname(file.originalname));
+      cb(null, uniqueName + extname(file.originalname));
     },
   }),
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5MB
+    fileSize: 5 * 1024 * 1024, 
+  },
+  fileFilter: (req, file, cb) => {
+    if (!file.mimetype.match(/\/(jpg|jpeg|png|webp)$/)) {
+      cb(new Error('Only image files allowed'), false);
+    }
+    cb(null, true);
   },
 };
--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,21 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
-
 async function bootstrap() {
-  const app =
-  await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-
-  app.useStaticAssets(
-  join(process.cwd(), 'uploads'),
-  { prefix: '/uploads' },
-);
+  app.useStaticAssets(join('/www/wwwroot/Civil_Resolve_backend/uploads'), {
+    prefix: '/uploads',
+  });
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   app.enableCors({
-    origin: "*",
+    origin: '*',
     credentials: true,
     methods: 'GET,POST,PUT,PATCH,DELETE,OPTIONS',
-    allowedHeaders: "Content-Type,Authorization",
-  })
+    allowedHeaders: 'Content-Type,Authorization',
+  });
   const config = new DocumentBuilder()
     .setTitle('Civil Resolve')
     .setDescription('The Civil Resolve API description')
