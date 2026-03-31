26951af3d0f2660c02fed46a890b87ddf301b590
saleem <ab588538@gmail.com>
2026-03-31 12:11:45 +0530

    fix teh dekploy error

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+import * as crypto from 'crypto';  
+(global as any).crypto = crypto;
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
